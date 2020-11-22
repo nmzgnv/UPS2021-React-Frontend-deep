c9b139f3629ab703b089cd3d5bcf0a4bb285d507
Nickolay Mozganov <nick-mozg1@mail.ru>
2020-11-22 20:34:53 +0500

    Login screen adaptive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import InputField from './Components/InputField'
+import LoginImage from './Images/LoginImage.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <h3>Welcome to a todo app</h3>
+          <InputField label="Email"></InputField>
+          <InputField label="Password"></InputField>
+          <button className="primary-button">Login</button>
+      </div>
+      <img className="media" src={LoginImage} alt='Image'></img>
     </div>
   );
 }
---
+++ src/Components/InputField.js
@@ -0,0 +1,11 @@
+const InputField = (props) => {
+    return (
+        <form>
+            <h4>{props.label}</h4>
+                <input type="Text" className="input-field"
+                        placeholder={"Enter "+ props.label + "..."}></input>
+        </form>
+    );
+  }
+
+export default InputField;
